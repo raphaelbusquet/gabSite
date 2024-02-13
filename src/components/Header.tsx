60e27122635d1503fded094f4c3b7501969d4d41
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-02-13 00:29:35 +0000

    Finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,20 +5,23 @@ import { logo, whatsapp } from "@/assets";
 const Header = () => {
   return (
     <SectionWrapper>
-        <div className="w-[1463px] flex justify-around items-center mt-[150px] mb-20 border border-[#0A0C0F] rounded-full bg-[#0A0C0F]">
-          <div>
-            <div className="w-[668px] h-[181px] flex justify-center items-center">
-              <Image src={logo} alt="logo" />
+        <div>
+          <div className="circleBlur z-0"></div>
+          <div className="w-[1463px] flex justify-around items-center mt-[150px] mb-20 border border-[#0A0C0F] rounded-full bg-[#0A0C0F] -z-10">
+            <div>
+              <div className="w-[668px] h-[181px] flex justify-center items-center">
+                <Image src={logo} alt="logo" className="z-10" />
+              </div>
             </div>
+            <div className="w-[795px] h-[181px] flex justify-center gap-12 items-center">
+                <div className="">
+                  <button className="cta">Quero um projeto!</button>
+                </div>
+                <div className="border-white border border-solid rounded-[15px]">
+                  <button className="flex justify-center items-center w-[136px] h-[73px]"><Image src={whatsapp} alt="whatsapp" className="w-[37px] h-[37px]" /></button>
+                </div>
+              </div>
           </div>
-          <div className="w-[795px] h-[181px] flex justify-center gap-12 items-center">
-              <div className="">
-                <button className="cta">Quero um projeto!</button>
-              </div>
-              <div className="border-white border border-solid rounded-[15px]">
-                <button className="flex justify-center items-center w-[136px] h-[73px]"><Image src={whatsapp} alt="whatsapp" className="w-[37px] h-[37px]" /></button>
-              </div>
-            </div>
         </div>
     </SectionWrapper>
   )

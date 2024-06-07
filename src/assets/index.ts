5e82259e3168bf58ec6220a4c1923a5d27e99d0b
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-06-07 19:11:31 +0000

    .

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,58 +1,57 @@
-import logo from "./images/logo.png"
-import whatsapp from "./images/Whatsapp.png"
-import instagram from "./images/instagram.png"
-import behance from "./images/behance.png"
-import linkedin from "./images/linkedin.png"
-import heroImage from "./images/heroImage.svg"
-import boxTextItem from "./images/boxTextItem.svg"
-import asterisk from "./images/asterisk.svg"
-import fileIcon from "./images/fileIcon.svg"
-import socialsBackground from "./images/socialsBackground.svg"
-import servicesIcon from "./images/servicesIcon.svg"
-import scrollDownIcon from "./images/scrollDownIcon.svg"
-import arrowLink from "./images/arrowLink.svg"
-import objectIconServices1 from "./images/objectIconServices1.svg"
-import objectIconServices2 from "./images/objectIconServices2.svg"
-import triangule from "./images/triangule.svg"
-import itemLeftArrow from "./images/itemLeftArrow.svg"
-import listItemSvg from "./images/listItemSvg.svg"
-import metalObject from "./images/metalObject.svg"
-import trianguleWorkProcess from "./images/trianguleWorkProcess.svg"
-import workProcessItem from "./images/workProcessItem.svg"
-import adicionalServicesArrow from "./images/adicionalServicesArrow.svg"
-import bigTriangule from "./images/bigTriangule.svg"
-import purpleBox from "./images/purpleBox.svg"
-import boxes from "./images/boxes.svg"
-import stacks from "./images/stacks.svg"
-import timer from "./images/timer.svg"
+import logo from "./images/logo.svg";
+import whatsapp from "./images/Whatsapp.svg";
+import instagram from "./images/instagram.svg";
+import behance from "./images/behance.svg";
+import linkedin from "./images/linkedin.svg";
+import heroImage from "./images/heroImage.svg";
+import boxTextItem from "./images/boxTextItem.svg";
+import asterisk from "./images/asterisk.svg";
+import fileIcon from "./images/fileIcon.svg";
+import socialsBackground from "./images/socialsBackground.svg";
+import servicesIcon from "./images/servicesIcon.svg";
+import scrollDownIcon from "./images/scrollDownIcon.svg";
+import arrowLink from "./images/arrowLink.svg";
+import objectIconServices1 from "./images/objectIconServices1.svg";
+import objectIconServices2 from "./images/objectIconServices2.svg";
+import triangule from "./images/triangule.svg";
+import itemLeftArrow from "./images/itemLeftArrow.svg";
+import listItemSvg from "./images/listItemSvg.svg";
+import metalObject from "./images/metalObject.svg";
+import trianguleWorkProcess from "./images/trianguleWorkProcess.svg";
+import workProcessItem from "./images/workProcessItem.svg";
+import adicionalServicesArrow from "./images/adicionalServicesArrow.svg";
+import bigTriangule from "./images/bigTriangule.svg";
+import purpleBox from "./images/purpleBox.svg";
+import boxes from "./images/boxes.svg";
+import stacks from "./images/stacks.svg";
+import timer from "./images/timer.svg";
 
 export {
-    logo,
-    whatsapp,
-    instagram,
-    behance,
-    linkedin,
-    heroImage,
-    boxTextItem,
-    asterisk,
-    fileIcon,
-    socialsBackground,
-    servicesIcon,
-    scrollDownIcon,
-    arrowLink,
-    objectIconServices1,
-    objectIconServices2,
-    triangule,
-    itemLeftArrow,
-    listItemSvg,
-    metalObject,
-    trianguleWorkProcess,
-    workProcessItem,
-    adicionalServicesArrow,
-    bigTriangule,
-    purpleBox,
-    boxes,
-    stacks,
-    timer
-
-}
+  logo,
+  whatsapp,
+  instagram,
+  behance,
+  linkedin,
+  heroImage,
+  boxTextItem,
+  asterisk,
+  fileIcon,
+  socialsBackground,
+  servicesIcon,
+  scrollDownIcon,
+  arrowLink,
+  objectIconServices1,
+  objectIconServices2,
+  triangule,
+  itemLeftArrow,
+  listItemSvg,
+  metalObject,
+  trianguleWorkProcess,
+  workProcessItem,
+  adicionalServicesArrow,
+  bigTriangule,
+  purpleBox,
+  boxes,
+  stacks,
+  timer,
+};

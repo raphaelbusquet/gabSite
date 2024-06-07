5727ed7e527fa21c5d28cd3e04cc6fe611556c85
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-06-07 22:20:15 +0100

    .

--- src/components/WorkProcess.tsx
+++ src/components/WorkProcess.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect } from 'react'; // Import useEffect hook
 import { trianguleWorkProcess, 
   workProcessItem,
   bigTriangule,
@@ -8,36 +11,76 @@ import { trianguleWorkProcess,
 } from "@/assets"
 import Image from "next/image"
 
-
 const WorkProcess = () => {
+  
+  // Use useEffect hook to execute code after component is mounted
+  useEffect(() => {
+    let cards = document.querySelectorAll(".card");
+      let stackArea = document.querySelector(".stack-area");
+
+      function rotateCards() {
+        let angle = 0;
+        cards.forEach((card) => {
+          if (card.classList.contains("active")) {
+            card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
+          } else {
+            card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+          }
+        });
+      }
+
+      rotateCards();
+
+      window.addEventListener("scroll", () => {
+        let proportion =
+          stackArea.getBoundingClientRect().top / window.innerHeight;
+        if (proportion <= 0) {
+          let n = cards.length;
+          let index = Math.ceil((proportion * n) / 2);
+          index = Math.abs(index) - 1;
+          for (let i = 0; i < n; i++) {
+            if (i <= index) {
+              cards[i].classList.add("active");
+            } else {
+              cards[i].classList.remove("active");
+            }
+          }
+          rotateCards();
+        }
+      });
+  }, []); // Empty dependency array ensures useEffect runs only once after initial render
+
   return (
     <div className="h-[1428px] w-screen flex justify-center bg-white">
       <div className="h-full w-[1463px] relative">
-        < div className="w-[500px] flex justify-center items-center gap-4 pt-32 pb-32">
-            <div className="h-8 w-8">
-              <Image src={trianguleWorkProcess} alt="Triangule" /> 
-            </div>
-            <h2 className="pt-4 text-black text-4xl">Processos de trabalho</h2>
+        <div className="w-[500px] flex justify-center items-center gap-4 pt-32 pb-32">
+          <div className="h-8 w-8">
+            <Image src={trianguleWorkProcess} alt="Triangule" /> 
+          </div>
+          <h2 className="pt-4 text-black text-4xl">Processos de trabalho</h2>
         </div>
         <div className="flex h-full">
           <div className="relative">
-              <Image src={bigTriangule} alt="Big Triangule" />
-              <Image className="absolute left-[10px] bottom-80px]" src={purpleBox} alt="Purple Box"/>
-              <Image className="absolute left-[5px] bottom-80px]" src={purpleBox} alt="Purple Box"/>
-              <Image className="absolute left-[0px] bottom-80px]" src={purpleBox} alt="Purple Box"/>
+            <Image src={bigTriangule} alt="Big Triangule" />
+            <Image className="absolute left-[210px] top-[-40px]" src={purpleBox} alt="Purple Box"/>
+            <Image className="absolute left-[-30px] top-[350px]" src={purpleBox} alt="Purple Box"/>
+            <Image className="absolute left-[450px] top-[350px]" src={purpleBox} alt="Purple Box"/>
+            <Image className="absolute left-[3px] top-[380px]" src={boxes} alt="Purple Box"/>
+            <Image className="absolute left-[483px] top-[380px]" src={stacks} alt="Purple Box"/>
+            <Image className="absolute left-[242px] top-[-10px]" src={timer} alt="Purple Box"/>
           </div>
-          <div className="text-black flex flex-col gap-8">
-            <div className="rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
+          <div className="text-black flex flex-col gap-8 stack-area">
+            <div className="card rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
               <h3 className="text-2xl text-primary">Alinhamento inicial</h3>
-              <p className="text-[22px] w-[540px]">Definimos cores, elementos e estilos para a peça, além de momentos para receber links essenciais para o projeto.</p>
+              <p className="text-[22px] w-[540px] text-[#3A3A3A]">Definimos cores, elementos e estilos para a peça, além de momentos para receber links essenciais para o projeto.</p>
             </div>
-            <div className="rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
+            <div className="card rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
               <h3 className="text-2xl text-primary">Desenvolvimento</h3>
-              <p className="text-[22px] w-[540px]">As entregas são feitas em &apos;blocos&apos;, garantindo a coerência. Alterações seguem o briefing e são solicitadas numa listagem para a aplicação simultânea, antes de reenviar a peça.</p>
+              <p className="text-[22px] w-[540px] text-[#3A3A3A]">As entregas são feitas em &apos;blocos&apos;, garantindo a coerência. Alterações seguem o briefing e são solicitadas numa listagem para a aplicação simultânea, antes de reenviar a peça.</p>
             </div>
-            <div className="rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
+            <div className="card rounded-[26px] w-[694px] h-[263px] bg-[#EEEDEF] flex flex-col gap-8 justify-center items-start text-justify pl-6">
               <h3 className="text-2xl text-primary">Prazos e Entrega</h3>
-              <p className="text-[22px] w-[540px]">Discutimos prazos, métodos de entrega e estabelecemos datas para garantir a assertividade na produtividade do projeto.</p>
+              <p className="text-[22px] w-[540px] text-[#3A3A3A]">Discutimos prazos, métodos de entrega e estabelecemos datas para garantir a assertividade na produtividade do projeto.</p>
             </div>
           </div>
         </div>
@@ -49,4 +92,4 @@ const WorkProcess = () => {
   )
 }
 
-export default WorkProcess 
+export default WorkProcess;

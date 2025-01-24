1d94a2b6932ecce340142506a9935f7fe71f1e22
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2025-01-24 02:06:42 +0000

    Update the next.config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,78 +4,42 @@ import "./globals.css";
 
 const glancyr = localFont({
   src: [
-    {
-      path: '../assets/fonts/Glancyr-thin.otf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-extra-light.otf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-light.otf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-medium.otf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-semibold.otf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/Glancyr-bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
+    { path: "../assets/fonts/Glancyr-thin.otf", weight: "100", style: "normal" },
+    { path: "../assets/fonts/Glancyr-extra-light.otf", weight: "200", style: "normal" },
+    { path: "../assets/fonts/Glancyr-light.otf", weight: "300", style: "normal" },
+    { path: "../assets/fonts/Glancyr-regular.otf", weight: "400", style: "normal" },
+    { path: "../assets/fonts/Glancyr-medium.otf", weight: "500", style: "normal" },
+    { path: "../assets/fonts/Glancyr-semibold.otf", weight: "600", style: "normal" },
+    { path: "../assets/fonts/Glancyr-bold.otf", weight: "700", style: "normal" },
   ],
 });
 
 export const metadata: Metadata = {
   title: "Gabriel Busquet - Web Designer | Crie seu site com a melhor qualidade e eficiência.",
-  description: "landing page de Gabriel Busquet", 
+  description: "Landing page de Gabriel Busquet",
+  icons: { icon: "/favicon.ico" },
   keywords: [
-    'designer gráfico',
-    'designer',
-    'criação de web design',
-    'figma especialista',
-    'frontend developer',
-    'landing page criar',
-    'fazer site',
-    'pagina de venda',
-    'infoproduto',
-    'webdesign',
-    'designer de sites',
-    'criador de sites',
-    'criador de sites profissional',
-    'melhor criador de sites',
+    "designer gráfico",
+    "designer",
+    "criação de web design",
+    "figma especialista",
+    "frontend developer",
+    "landing page criar",
+    "fazer site",
+    "pagina de venda",
+    "infoproduto",
+    "webdesign",
+    "designer de sites",
+    "criador de sites",
+    "criador de sites profissional",
+    "melhor criador de sites",
   ],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="../../public/favicon.ico" />
-      </head>
-      <body className={glancyr.className}>
-        {children}
-      </body>
+      <body className={glancyr.className}>{children}</body>
     </html>
   );
 }
